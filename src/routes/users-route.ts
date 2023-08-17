aa9c621aefe12abc4d77571ae0444a515e82493b
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-17 13:15:12 +0100

    feat: add sign up validation to route

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -1,24 +1,46 @@
 import express from 'express';
 const router = express.Router();
 
-import  { signUpUser }   from '../controllers/users-controller';
-
+import  { signUpUser }   from '../controllers/auth-controller';
+import  signupSchema  from  '../validators/auth-validator';
 import { StatusCodes } from 'http-status-codes'; 
 
 
 router.post('/signup', async (req, res) => {
     const newUserSignupData = req.body;
-
+  
     try {
-        const user = await signUpUser(newUserSignupData);
-        res.status(StatusCodes.CREATED).json({ user });
+    
+      const { error, value } = signupSchema.validate(newUserSignupData);
+  
+      if (error) {
+        
+        res.status(StatusCodes.BAD_REQUEST).json({ errors: error.details });
+        return;
+      }
+      const user = await signUpUser(value);
+      res.status(StatusCodes.CREATED).json({ user });
     } catch (error) {
-        const errorMessage = (error as Error).message;
-        res.status(StatusCodes.BAD_REQUEST).json({  message: errorMessage });
+      const errorMessage = (error as Error).message;
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: errorMessage });
     }
-});
+  });
+  
+  export default router;
 
-export default router;
+// router.post('/signup', async (req, res) => {
+//     const newUserSignupData = req.body;
+
+//     try {
+//         const user = await signUpUser(newUserSignupData);
+//         res.status(StatusCodes.CREATED).json({ user });
+//     } catch (error) {
+//         const errorMessage = (error as Error).message;
+//         res.status(StatusCodes.BAD_REQUEST).json({  message: errorMessage });
+//     }
+// });
+
+// export default router;
 
 
 
