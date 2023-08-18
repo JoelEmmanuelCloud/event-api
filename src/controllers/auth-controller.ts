5666bb2e2eb7c675b6f6d7bc299ec235c859f80b
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-18 04:20:44 +0100

    feat: add sign in controller

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,10 +1,10 @@
 import CustomError from '../error';
 import UserModel  from '../models/users';
-import { SignupData, IUserDocument } from '../interfaces/userTypes'
+import { SignupData, IUserDocument, SigninData } from '../interfaces/userTypes'
 
 
 
-export async function signUpUser(signupData: SignupData): Promise<IUserDocument> {
+async function signUpUser(signupData: SignupData): Promise<IUserDocument> {
     try {
         
         const existingUser = await UserModel.findOne({ email: signupData.email });
@@ -25,4 +25,30 @@ export async function signUpUser(signupData: SignupData): Promise<IUserDocument>
 }
 
 
+async function signinUser(signinData: SigninData): Promise<IUserDocument> {
+    try {
+        const { email, password } = signinData;
 
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+            throw new CustomError.NotFoundError('User not found');
+        }
+
+        const isPasswordValid = await user.comparePassword(password);
+
+        if (!isPasswordValid) {
+            throw new CustomError.UnauthorizedError('Invalid password');
+        }
+
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+export {
+    signUpUser,
+    signinUser
+}
