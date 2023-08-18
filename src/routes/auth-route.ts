47cfe26efb0b591bc2330f99cc0a45139b8eae83
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-18 10:53:19 +0100

    feat: add jwt verification to sing in route

--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-
+import { createJWT } from '../utils/jwt';
 import  { signUpUser, signinUser }   from '../controllers/auth-controller';
 import {signupSchema, signinSchema}  from  '../validators/auth-validator'
 import { StatusCodes } from 'http-status-codes'; 
@@ -42,13 +42,15 @@ router.post('/signin', async (req, res) => {
 
         const user = await signinUser(value);
 
+        const token = createJWT( {payload: { _id: user._id}} );
+
         const userResponse = {
             firstName: user.firstName,
             lastName: user.lastName,
             email: user.email
         };
 
-        res.status(StatusCodes.OK).json({ user: userResponse });
+        res.status(StatusCodes.OK).json({ token, user: userResponse });
     } catch (error) {
         const errorMessage = (error as Error).message;
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: errorMessage });
