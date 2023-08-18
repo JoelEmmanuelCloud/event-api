add13f10d9910e4ffd66199df46832ea15ca2a2c
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-18 10:45:59 +0100

    feat: set up jwt payload

---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import { Payload } from '../interfaces/userTypes';
+
+
+const createJWT = ({ payload }: { payload: Payload }): string => {
+    const token = jwt.sign(payload, process.env.JWT_SECRET!, {
+        expiresIn: process.env.JWT_LIFETIME!,
+    });
+    return token;
+};
+
+const isTokenValid = ({ token }: { token: string }): Payload => {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as Payload;
+    return decodedToken;
+};
+
+
+export {
+    createJWT,
+    isTokenValid
+}
