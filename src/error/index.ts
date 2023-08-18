d5c5f5429f287834ab981e049edce7e1079b2065
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-18 04:22:40 +0100

    feat: add new error handlers to custom errors

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,13 +1,13 @@
 import CustomAPIError from './custom-api';
-// import UnauthenticatedError from './unauthenticated';
+import UnauthenticatedError from './unauthenticated';
 import NotFoundError from './not-found';
 import BadRequestError from './bad-request';
-// import UnauthorizedError from './unauthorized';
+import UnauthorizedError from './unauthorized';
 
 export default {
     CustomAPIError,
-    // UnauthenticatedError,
+    UnauthenticatedError,
     NotFoundError,
     BadRequestError,
-    // UnauthorizedError,
+    UnauthorizedError,
 };
