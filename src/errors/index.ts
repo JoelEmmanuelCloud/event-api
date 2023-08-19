68273993775778541a71977e423e14ee7c58bced
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-19 09:34:02 +0100

    fix: custom error

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,12 +1,10 @@
 import CustomAPIError from './custom-api';
-import UnauthenticatedError from './unauthenticated';
 import NotFoundError from './not-found';
 import BadRequestError from './bad-request';
 import UnauthorizedError from './unauthorized';
 
 export default {
     CustomAPIError,
-    UnauthenticatedError,
     NotFoundError,
     BadRequestError,
     UnauthorizedError,
