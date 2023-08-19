9bd16a9ac4b7fee9fc3c86ea5f9708975a857653
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-19 04:10:38 +0100

    feat: delete event by day endpoint

--- src/routes/event-route.ts
+++ src/routes/event-route.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { createEvent, getEvents } from '../controllers/event-controller';
+import { createEvent, getEvents, deleteEventsByDay } from '../controllers/event-controller';
 import { authenticateUser, ExtendedRequest } from '../middleware/authenticateUser';
-import { eventSchema } from '../validators/event-validator';
+import { createEventSchema } from '../validators/event-validator';
 
 const router = express.Router();
 
 router.post('/createEvent', authenticateUser, async (req: ExtendedRequest, res) => {
     try {
-        const { error } = eventSchema.validate(req.body);
+        const { error } = createEventSchema.validate(req.body);
 
         if (error) {
             const errorMessage = error.details.map((detail) => detail.message).join(', ');
@@ -27,6 +27,16 @@ router.get('/getEvents', authenticateUser, async (req: ExtendedRequest, res) => 
     try {
         const events = await getEvents(req, res);
         res.status(StatusCodes.OK).json(events);
+    } catch (error) {
+        const errorMessage = (error as Error).message;
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: errorMessage });
+    }
+});
+
+router.delete('/deleteEvents/:dayOfWeek', authenticateUser, async (req: ExtendedRequest, res) => {
+    try {
+        const deletedEventsResponse = await deleteEventsByDay(req, res);
+        res.status(StatusCodes.OK).json(deletedEventsResponse);
     } catch (error) {
         const errorMessage = (error as Error).message;
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: errorMessage });
