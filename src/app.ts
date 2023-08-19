7c388024ed69fef31b0ae5271e47d25ddf56c351
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-19 03:19:27 +0100

    feat: route events endpoints to server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDB from './db/connectDB';
 import authRouter from './routes/auth-route';
+import eventRouter from './routes/event-route';
 import notFoundMiddleware from './middleware/not-found';
 import errorHandlerMiddleware from './middleware/error-handler';
 import dotenv from 'dotenv';
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/events', eventRouter);
 app.get('/', (req, res) => {
 
     res.send('Hello, Express!');
