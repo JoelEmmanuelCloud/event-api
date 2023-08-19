806797a4bb820bb9d9569acf42941ad0e587c50a
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-19 04:11:11 +0100

    refactor: change code structure

--- src/validators/event-validator.ts
+++ src/validators/event-validator.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi';
 
-export const eventSchema = Joi.object({
+export const createEventSchema = Joi.object({
     description: Joi.string().required().max(255).label('Description'),
     dayOfWeek: Joi.string().required().valid('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday').label('Day of Week')
 });
+
