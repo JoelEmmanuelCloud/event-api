00a4ee9f087e9cc3f1c8c5a70949273aa4089d2c
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-19 03:00:49 +0100

    feat: add user id to the event schema

---
+++ src/models/events.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Model } from 'mongoose';
+import { IEventDocument } from '../interfaces/eventTypes';
+
+
+const EventSchema: Schema<IEventDocument> = new Schema(
+    {
+        description: {
+            type: String,
+            required: [true, 'Please provide a description'],
+        },
+        dayOfWeek: {
+            type: String,
+            required: [true, 'Please provide the day of the week'],
+        },
+        userId: { // Add this field to your schema
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export interface IEventModel extends Model<IEventDocument> {}
+
+export const EventModel: IEventModel = mongoose.model<IEventDocument, IEventModel>('Event', EventSchema);
