ea60f7c7c2df51a840e02e730ffa2cd7ed8a0d46
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-19 09:34:14 +0100

    fix: custom error

--- src/errors/unauthorized.ts
+++ src/errors/unauthorized.ts
@@ -2,12 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import CustomAPIError from './custom-api';
 
 class UnauthorizedError extends CustomAPIError {
-  statusCode: number;
+    statusCode: number;
 
-  constructor(message: string) {
-    super(message);
-    this.statusCode = StatusCodes.FORBIDDEN;
-  }
+    constructor(message: string) {
+        super(message);
+        this.statusCode = StatusCodes.FORBIDDEN;
+    }
 }
 
 export default UnauthorizedError;
