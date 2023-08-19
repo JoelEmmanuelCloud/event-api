f1bd99fc4886c6698c7537edd3819b9de80b48f2
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-19 02:59:24 +0100

    feat: create event validator

---
+++ src/validators/event-validator.ts
@@ -0,0 +1,6 @@
+import Joi from 'joi';
+
+export const eventSchema = Joi.object({
+    description: Joi.string().required().max(255).label('Description'),
+    dayOfWeek: Joi.string().required().valid('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday').label('Day of Week')
+});
