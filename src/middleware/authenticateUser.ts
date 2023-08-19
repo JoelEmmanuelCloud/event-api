a0f9f25a091cac3adc1010d6a1ad8c39b95e9ed0
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-19 03:02:59 +0100

    fix: missing token error

---
+++ src/middleware/authenticateUser.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+import { isTokenValid } from '../utils/jwt';
+import UnauthenticatedError from '../errors/unauthenticated';
+import { StatusCodes } from 'http-status-codes';
+
+
+export interface ExtendedRequest extends Request {
+    userId?: string;
+}
+
+const authenticateUser = async (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+//   if (!token) {
+//     throw new UnauthenticatedError('Unauthorized Access');
+//   }
+
+if (!token) {
+    res.status(StatusCodes.UNAUTHORIZED).json({
+        error: 'Authentication token missing',
+    });
+    return;
+}
+
+  try {
+    const payload = isTokenValid({ token });
+    req.userId = payload._id; 
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { authenticateUser };
