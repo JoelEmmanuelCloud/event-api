1b1b9043944d7a8abeb40b0ed1efd44939bbc346
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-21 10:36:56 +0100

    build: install jest collectCoverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.test.(ts|js)'],
+    collectCoverage: true,
     verbose: true,
     forceExit: true,
   };
