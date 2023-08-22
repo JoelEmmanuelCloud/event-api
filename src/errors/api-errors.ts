a0c5c321441f64f0597d43f791d92ed8ea4d300e
Joel Emmanuel <ejoel0035@gmail.com>
2023-08-22 08:55:45 +0100

    feat: declared error classes in the same file.

---
+++ src/errors/api-errors.ts
@@ -0,0 +1,41 @@
+import { StatusCodes } from 'http-status-codes';
+
+class CustomAPIError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+class BadRequestError extends CustomAPIError {
+    statusCode: number;
+
+    constructor(message: string) {
+        super(message);
+        this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+}
+
+class NotFoundError extends CustomAPIError {
+    statusCode: number;
+
+    constructor(message: string) {
+        super(message);
+        this.statusCode = StatusCodes.NOT_FOUND;
+    }
+}
+
+class UnauthorizedError extends CustomAPIError {
+    statusCode: number;
+
+    constructor(message: string) {
+        super(message);
+        this.statusCode = StatusCodes.FORBIDDEN;
+    }
+}
+
+export {
+    CustomAPIError,
+    BadRequestError,
+    NotFoundError,
+    UnauthorizedError,
+};
