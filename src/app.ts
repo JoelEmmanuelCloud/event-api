43e0c3b94e163d71d1e76d4b6c22382d19c7ca1a
Joel Emmanuel <123770803+JoelEmmanuelCloud@users.noreply.github.com>
2023-08-28 09:36:36 +0100

    style: remove unused code

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use('/api/v1/events', eventRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = Number(process.env.PORT) || 5000;
+const port = Number(process.env.PORT);
 
 const MONGO_URL = process.env.MONGO_URL as string;
 
